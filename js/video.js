66c9c912a9d3d96989d623141868904f05d451fa
gemut1ich <59188183+gemut1ich@users.noreply.github.com>
2022-04-09 20:01:42 -0400

    fix typo of player id

--- js/video.js
+++ js/video.js
@@ -1,18 +1,13 @@
 var video;
 
-window.addEventListener("load", function() {
-	console.log("Good job opening the window")
-
+window.addEventListener("load", function () {
+  console.log("Good job opening the window");
 });
 
 // When loading page, Initialize the video element and turn off autoplay and turn off looping.
 window.addEventListener("load", () => {
-		video = document.getElementById("video");
-		video.autoplay = false;
-		video.loop = false;
-	});
-
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
-
+  video = document.getElementById("player1");
+  console.log("video: ", video);
+  video.autoplay = false;
+  video.loop = false;
+});
